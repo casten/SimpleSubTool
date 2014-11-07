504a12e955c35915851129cf6cd3d291d90dcd14
Casten <criepling@netflix.com>
2014-11-06 19:09:16 -0800

    added

---
+++ 3.6.5.py
@@ -0,0 +1,54 @@
+from bitarray import bitarray
+
+
+def step(array,taps,shift):
+	t = 0
+	for tap in taps:
+		t ^= array[tap]
+	for (i in range(shift,0,-1):
+		array[i]=array[i-1]
+	array[0] = t
+
+def xStep(array):
+	step(array,[13,16,17,18],18)
+
+def yStep(array):
+	step(array,[20,21],21)
+
+def zStep(array):
+	step(array,[7,20,21,22],22)
+
+def getMajority(x,y,z):
+	return (x[0] + y[0] + z[0]) > 1?1:0
+
+def stepRegistersAsNeeded(x,y,z,maj):
+	if (x[0] == maj):
+		xStep(x)
+	if (y[0] == maj):
+		yStep(y)
+	if (z[0] == maj):
+		zStep(z)
+
+def getBit(x,y,z):
+	return x[18]^y[21]^y[22]
+
+
+def main():
+	x = bitarray('1010101010101010101')
+	y = bitarray('1100110011001100110011')
+	z = bitarray('11100001111000011110000')
+	cs = bitarray(64)
+	for i in range (0,64):
+		maj = getMajority(x,y,z)
+		stepRegistersAsNeeded(x,y,z,maj)
+		cs[i] = getBit(x,y,z)
+	print 'cs = '+cs
+	print 'x = '+x
+	print 'y = '+y
+	print 'z = '+z
+
+if __name__ == "__main__":
+    main()
+
+
+
